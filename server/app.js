107918f52a069e89e10c8f5463713d0edbf7e288
wiji <wijigaming.yt@gmail.com>
2024-11-03 00:26:17 -0700

    additional logging

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ const router = require('./routes');
 const { setupWebSocket } = require('./services/websocketService');
 const { databaseInit } = require('./controllers/database');
 const { authInit } = require('./controllers/authentication');
+const path = require("path");
+const {exec} = require("child_process");
 
 const app = express();
 const server = http.createServer(app);
@@ -23,7 +25,26 @@ setupWebSocket(server);
 // Initialize other services
 databaseInit();
 authInit();
+installDependencies();
 
 server.listen(3001, () => {
     console.log('Server is listening on port 3001');
 });
+
+
+async function installDependencies() {
+    const { exec } = require('child_process');
+    const scriptDir = path.join(config["bmc-path"], "scripts");
+
+    console.log('Installing dependencies...');
+
+    exec(`cd ${scriptDir} && ls && ./install-dependents.sh`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+
+        // console.log(`stdout: ${stdout}`);
+        console.error(`Failed to install dependencies: ${stderr}`);
+    });
+}
