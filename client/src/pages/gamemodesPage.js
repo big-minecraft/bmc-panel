6263f7b236c0fa83dd87943d757d38e2ddf4a153
wiji <wijigaming.yt@gmail.com>
2024-11-19 15:37:51 -0800

    test basic proxy config implementation

--- client/src/pages/gamemodesPage.js
+++ client/src/pages/gamemodesPage.js
@@ -215,11 +215,22 @@ const GamemodesPage = () => {
                     <div className="card">
                         <div className="card-body d-flex justify-content-between align-items-center">
                             <div>
-                                <h5 className="card-title mb-1">Proxy Configuration</h5>
+                                <h5 className="card-title mb-1">Velocity Proxy</h5>
                                 <p className="card-text text-muted small mb-0">{proxyConfig.path}</p>
                             </div>
 
                             <div className="d-flex align-items-center gap-3">
+                                <button
+                                    className="btn btn-outline-info btn-sm"
+                                    onClick={() => handleViewData(proxy.dataDirectory)}
+                                    title="View Data Directory"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         className="bi bi-folder" viewBox="0 0 16 16">
+                                        <path
+                                            d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
+                                    </svg>
+                                </button>
                                 <button
                                     className="btn btn-outline-warning btn-sm"
                                     onClick={handleRestartProxy}
@@ -227,11 +238,15 @@ const GamemodesPage = () => {
                                     disabled={restartingProxy}
                                 >
                                     {restartingProxy ? (
-                                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+                                        <span className="spinner-border spinner-border-sm" role="status"
+                                              aria-hidden="true"/>
                                     ) : (
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                                            <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
-                                            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                             fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                                            <path fillRule="evenodd"
+                                                  d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                                            <path
+                                                d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
                                         </svg>
                                     )}
                                 </button>
@@ -240,12 +255,14 @@ const GamemodesPage = () => {
                                     className="btn btn-outline-primary btn-sm"
                                     onClick={handleEditProxy}
                                 >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
-                                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         className="bi bi-pencil" viewBox="0 0 16 16">
+                                        <path
+                                            d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                                     </svg>
                                 </button>
 
-                                <div className="form-check form-switch" style={{ minWidth: '120px' }}>
+                                <div className="form-check form-switch" style={{minWidth: '120px'}}>
                                     <input
                                         className="form-check-input"
                                         type="checkbox"
