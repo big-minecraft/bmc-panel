fa8746046336c76ede21c89354942c8c19c369fe
wiji <wijigaming.yt@gmail.com>
2024-11-19 18:25:47 -0800

    send proxy updates

--- server/controllers/gamemodes.js
+++ server/controllers/gamemodes.js
@@ -5,6 +5,7 @@ const yaml = require('js-yaml');
 const {scaleDeployment} = require("./k8s");
 const {createSFTPDirectory, deleteSFTPDirectory} = require("./sftp");
 const util = require('util');
+const {sendGamemodeUpdate} = require("./redis");
 const exec = util.promisify(require('child_process').exec);
 
 async function getGamemodes() {
