c6584e0b4b17395ee84343d34d4597bfa9ea01e7
wiji <wijigaming.yt@gmail.com>
2024-11-22 17:14:25 -0800

    Revert "try making no resources cards"
    This reverts commit 2bace0fbb25b66116e66712c6c61511263c02028.

--- client/src/pages/databasesPage.js
+++ client/src/pages/databasesPage.js
@@ -117,29 +117,6 @@ const DatabasesPage = () => {
         }));
     };
 
-    const EmptyState = () => (
-        <div className="col-12">
-            <div className="card">
-                <div className="card-body text-center py-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-database text-muted mb-3" viewBox="0 0 16 16">
-                        <path d="M4.318 2.687C5.234 2.271 6.536 2 8 2s2.766.27 3.682.687C12.644 3.125 13 3.627 13 4c0 .374-.356.875-1.318 1.313C10.766 5.729 9.464 6 8 6s-2.766-.27-3.682-.687C3.356 4.875 3 4.373 3 4c0-.374.356-.875 1.318-1.313ZM13 5.698V7c0 .374-.356.875-1.318 1.313C10.766 8.729 9.464 9 8 9s-2.766-.27-3.682-.687C3.356 7.875 3 7.373 3 7V5.698c.271.202.58.378.904.525C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 5.698ZM14 4c0-1.007-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1s-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4v9c0 1.007.875 1.755 1.904 2.223C4.978 15.71 6.427 16 8 16s3.022-.289 4.096-.777C13.125 14.755 14 14.007 14 13V4Zm-1 4.698V10c0 .374-.356.875-1.318 1.313C10.766 11.729 9.464 12 8 12s-2.766-.27-3.682-.687C3.356 10.875 3 10.373 3 10V8.698c.271.202.58.378.904.525C4.978 9.71 6.427 10 8 10s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 8.698Zm0 3V13c0 .374-.356.875-1.318 1.313C10.766 14.729 9.464 15 8 15s-2.766-.27-3.682-.687C3.356 13.875 3 13.373 3 13v-1.302c.271.202.58.378.904.525C4.978 12.71 6.427 13 8 13s3.022-.289 4.096-.777c.324-.147.633-.323.904-.525Z"/>
-                    </svg>
-                    <h3 className="text-muted mb-2">No Databases Found</h3>
-                    <p className="text-muted mb-4">Get started by creating your first database.</p>
-                    <button
-                        className="btn btn-primary d-inline-flex align-items-center gap-2"
-                        onClick={() => setShowCreateModal(true)}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
-                        </svg>
-                        Create Database
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
-
     if (isLoading) {
         return (
             <div className="min-vh-100 d-flex align-items-center justify-content-center">
@@ -180,20 +157,6 @@ const DatabasesPage = () => {
                 >
                     Create Database
                 </button>
-            </div>
-
-            <div>
-                <div className="row g-4">
-                    {databases.length === 0 ? (
-                        <EmptyState />
-                    ) : (
-                        databases.map((database) => (
-                            <div key={database.name} className="col-12">
-                                <div className="card"></div>
-                            </div>
-                        ))
-                    )}
-                </div>
             </div>
 
             <div>
