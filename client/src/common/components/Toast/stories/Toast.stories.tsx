92c5bd1277cc953fe90b5c1c206d7bba172743d3
Timothy <timothyrwashburn@gmail.com>
2024-12-19 00:16:41 -0800

    modify common components

--- client/src/common/components/Toast/stories/Toast.stories.tsx
+++ client/src/common/components/Toast/stories/Toast.stories.tsx
@@ -90,12 +90,6 @@ const ToastDemo = ({
                         Show Info
                     </button>
                     <button
-                        onClick={() => success(message, title)}
-                        className="w-full px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-600"
-                    >
-                        Show Custom Toast
-                    </button>
-                    <button
                         onClick={clearToasts}
                         className="w-full px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
                     >
