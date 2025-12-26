6699a4c6ffb407299b959dd2915a2addd656327b
wiji <wijigaming.yt@gmail.com>
2025-12-25 20:41:30 -0700

    skipawait on file session startup

--- server/src/services/pulumi/pulumiDeploymentService.ts
+++ server/src/services/pulumi/pulumiDeploymentService.ts
@@ -434,7 +434,7 @@ export class PulumiDeploymentService {
                     username: "user",
                     password: "password",
                     port: 22,
-                    rootPath: "data",
+                    rootPath: "/",
                     pvcName: pvcName,
                     nodePort: sftpPort
                 }
